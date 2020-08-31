cf71baa2a7b10201200747d9ad39f55b64a896fd
Andrew Chisholm <flatlinerdoa@gmail.com>
2020-08-31 13:59:18 +1000

    Improving the sample app

--- NAPS2.Wia.WpfSample/MainWindow.xaml.cs
+++ NAPS2.Wia.WpfSample/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Drawing;
@@ -17,7 +19,7 @@ namespace NAPS2.Wia.WpfSample
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private string currentDevice;
+        private WiaDeviceInfo currentDevice;
         private string currentPaperSource;
         private bool hasLoadedDevices = false;
 
@@ -29,36 +31,16 @@ namespace NAPS2.Wia.WpfSample
             this.DataContext = this;
         }
 
-
-        protected override void OnActivated(EventArgs e)
-        {
-            base.OnActivated(e);
-            if (!this.hasLoadedDevices)
-            {
-                this.hasLoadedDevices = true;
-                this.Devices.Clear();
-                using (var deviceManager = new WiaDeviceManager())
-                {
-                    foreach (var device in deviceManager.GetDeviceInfos())
-                    {
-                        using (device)
-                        {
-                            this.Devices.Add(device.Id());
-                        }
-                    }
-                }
-
-                this.CurrentDevice = this.Devices.FirstOrDefault();
-            }
-        }
-
-        public ObservableCollection<string> Devices { get; } = new ObservableCollection<string>();
+        public ObservableCollection<WiaDeviceInfo> Devices { get; } = new ObservableCollection<WiaDeviceInfo>();
 
         public ObservableCollection<string> PaperSources { get; } = new ObservableCollection<string>();
 
+
         public ObservableCollection<ImageSource> Pages { get; } = new ObservableCollection<ImageSource>();
 
-        public string CurrentDevice
+        public ObservableCollection<string> Logs { get; } = new ObservableCollection<string>();
+
+        public WiaDeviceInfo CurrentDevice
         {
             get => this.currentDevice;
             set
@@ -66,36 +48,23 @@ namespace NAPS2.Wia.WpfSample
                 if (this.currentDevice != value)
                 {
                     this.currentDevice = value;
-                    this.RefreshDeviceCapabilities(value);
                     this.RaisePropertyChanged();
+
+                    this.PaperSources.Clear();
+                    foreach (var source in this.currentDevice.Sources)
+                    {
+                        this.PaperSources.Add(source);
+                    }
+
+                    this.CurrentPaperSource = this.PaperSources.FirstOrDefault();
                 }
             }
         }
 
-        private void RefreshDeviceCapabilities(string deviceId)
-        {
-            this.PaperSources.Clear();
-            using (var deviceManager = new WiaDeviceManager())
-            {
-                using (var device = deviceManager.FindDevice(deviceId))
-                {
-                    if (device.SupportsFlatbed())
-                    {
-                        this.PaperSources.Add("Flatbed");
-                    }
-
-                    if (device.SupportsFeeder())
-                    {
-                        this.PaperSources.Add("Feeder");
-                        if (device.SupportsDuplex())
-                        {
-                            this.PaperSources.Add("FeederDuplex");
-                        }
-                    }
-                }
-            }
-        }
-
+        /// <summary>
+        /// Gets or sets what kind of scan we are going to perform, from a flatbed, 
+        /// or a feeder and, whether it should be single or double sided (DUPLEX)
+        /// </summary>
         public string CurrentPaperSource
         {
             get => this.currentPaperSource;
@@ -104,32 +73,118 @@ namespace NAPS2.Wia.WpfSample
                 if (this.currentPaperSource != value)
                 {
                     this.currentPaperSource = value;
+                    this.RaisePropertyChanged();
                 }
             }
         }
 
-        public ObservableCollection<string> Logs { get; } = new ObservableCollection<string>();
-
-        private void ScanClicked(object sender, RoutedEventArgs e)
+        protected override void OnActivated(EventArgs e)
         {
-            this.Pages.Clear();
-            this.Log("Starting WIA Scan");
+            base.OnActivated(e);
+            if (!this.hasLoadedDevices)
+            {
+                this.hasLoadedDevices = true;
+                this.RefreshDevices();
+            }
+        }
+
+        private void RefreshDevices()
+        {
+            this.Devices.Clear();
+
+            var devices = this.GetAllDevices();
+            foreach (var device in devices)
+            {
+                this.Devices.Add(device);
+            }
+
+            this.CurrentDevice = this.Devices.FirstOrDefault();
+        }
+
+        private IReadOnlyList<WiaDeviceInfo> GetAllDevices()
+        {
+            var devices = new List<WiaDeviceInfo>();
             try
             {
                 using (var deviceManager = new WiaDeviceManager(WiaVersion.Wia20))
                 {
-                    using (var device = deviceManager.FindDevice(this.CurrentDevice))
+                    foreach (var device in deviceManager.GetDeviceInfos())
+                    {
+                        try
+                        {
+
+                            using (device)
+                            {
+
+                                var paperSources = new List<string>();
+                                using (var openDevice = deviceManager.FindDevice(device.Id()))
+                                {
+                                    if (openDevice.SupportsFlatbed())
+                                    {
+                                        paperSources.Add("Flatbed");
+                                    }
+
+                                    if (openDevice.SupportsFeeder())
+                                    {
+                                        paperSources.Add("Feeder");
+                                        if (openDevice.SupportsDuplex())
+                                        {
+                                            paperSources.Add("FeederDuplex");
+                                        }
+                                    }
+                                }
+
+                                var info = new WiaDeviceInfo(device.Id(), device.Name(), paperSources);
+                                devices.Add(info);
+                            }
+                        }
+                        catch (WiaException ex)
+                        {
+                            this.Log(ex.Message);
+                        }
+                    }
+                }
+            }
+            catch (WiaException ex)
+            {
+                this.Log(ex.Message);
+            }
+
+            return devices;
+        }
+
+        private void ScanClicked(object sender, RoutedEventArgs e)
+        {
+            this.Pages.Clear();
+            Task.Factory.StartNew(this.Scan, TaskCreationOptions.LongRunning);
+        }
+
+        private void Scan()
+        {
+            try
+            {
+                this.Log("Starting WIA Scan");
+                using (var deviceManager = new WiaDeviceManager(WiaVersion.Wia20))
+                {
+                    using (var device = deviceManager.FindDevice(this.CurrentDevice.Id))
                     {
                         var subItem = this.CurrentPaperSource == "Flatbed" ? "Flatbed" : "Feeder";
 
                         // Select between Flatbed/Feeder
-                        using (var item = device.FindSubItem(subItem)) {
+                        using (var item = device.FindSubItem(subItem))
+                        {
 
                             if (this.CurrentPaperSource == "FeederDuplex")
                             {
                                 // | WiaPropertyValue.FRONT_FIRST
                                 // Enable duplex scanning
                                 item.SetProperty(WiaPropertyId.IPS_DOCUMENT_HANDLING_SELECT, WiaPropertyValue.DUPLEX);
+                            }
+
+                            if (subItem == "Feeder")
+                            {
+                                // Set WIA_IPS_PAGES to 0 to scan all pages.
+                                item.SetProperty(WiaPropertyId.IPS_PAGES, 0);
                             }
 
                             // Set up the scan
@@ -152,7 +207,7 @@ namespace NAPS2.Wia.WpfSample
                     }
                 }
             }
-            catch (Exception error)
+            catch (WiaException error)
             {
                 this.Log(error.Message);
             }
@@ -204,6 +259,27 @@ namespace NAPS2.Wia.WpfSample
         private void RaisePropertyChanged([CallerMemberName] string propertyName = "")
         {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Selection object for device id, name and what it can do.
+        /// </summary>
+        public sealed class WiaDeviceInfo
+        {
+            public WiaDeviceInfo(string id, string name, IEnumerable<string> supportedPaperSources)
+            {
+                this.Id = id;
+                this.Name = name;
+                foreach (var source in supportedPaperSources)
+                {
+                    this.Sources.Add(source);
+                }
+            }
+            public string Id { get; }
+
+            public string Name { get; }
+
+            public List<string> Sources { get; } = new List<string>();
         }
     }
 }
--- NAPS2.Wia/WiaException.cs
+++ NAPS2.Wia/WiaException.cs
@@ -19,11 +19,42 @@ namespace NAPS2.Wia
         {
         }
 
-        public WiaException(uint errorCode) : base($"WIA error code {errorCode:X}")
+        public WiaException(uint errorCode) : base(MessageForErrorCode(errorCode))
         {
             ErrorCode = errorCode;
         }
 
         public uint ErrorCode { get; set; }
+
+        public static string MessageForErrorCode(uint errorCode)
+        {
+            switch (errorCode)
+            {
+                case WiaErrorCodes.PAPER_EMPTY:
+                    return "Paper empty";
+                case WiaErrorCodes.NO_DEVICE_AVAILABLE:
+                    return "No device available";
+                case WiaErrorCodes.OFFLINE:
+                    return "The device is offline";
+                case WiaErrorCodes.PAPER_JAM:
+                    return "Paper jam";
+                case WiaErrorCodes.BUSY:
+                    return "Device is busy";
+                case WiaErrorCodes.COVER_OPEN:
+                    return "Paper cover is open";
+                case WiaErrorCodes.COMMUNICATION:
+                    return "Device communication error";
+                case WiaErrorCodes.LOCKED:
+                    return "Device is locked";
+                case WiaErrorCodes.INCORRECT_SETTING:
+                    return "Incorrect setting";
+                case WiaErrorCodes.LAMP_OFF:
+                    return "Lamp is off";
+                case WiaErrorCodes.WARMING_UP:
+                    return "Device is warming up";
+            }
+
+            return $"WIA error code {errorCode:X}";
+        }
     }
 }
